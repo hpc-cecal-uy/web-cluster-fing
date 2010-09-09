ffc46761ee64b771f743ede3f50a51db4de0359c
santiago.iturriaga <santiago.iturriaga@2c19abef-57cf-ded0-be5c-1a4e2597ff9d>
2010-09-09 14:00:15 +0000

    Se actualiza link de PDF de como conectarse a AmbientePVM

--- system/application/views/como_conectarse.php
+++ system/application/views/como_conectarse.php
@@ -11,7 +11,7 @@
 						<li>Se genera con comandos de ssh (ssh-keygen) y utilitarios (putty-keygen).</li>
 						<li>Clave RSA de 1024 bits.</li>
 						<li>La clave pública se debe enviar por correo electrónico, y la clave privada se almacena en un archivo accesible al(a los) equipo(s) desde los cuales se establecerá la conexión.</li>
-						<li>El procedimiento de generación de claves puede consultarse en el siguiente <a href="http://www.fing.edu.uy/inco/cursos/hpc/material/clases/hpc2008-ambiente_pvm.pdf">enlace</a> y en sitios de Internet.</li>
+						<li>El procedimiento de generación de claves puede consultarse en el siguiente <a href="http://www.fing.edu.uy/inco/cursos/hpc/material/clases/AmbientePVM.pdf">enlace</a> y en sitios de Internet.</li>
 					</ul>
 				</li>
 				</ul>
